7d06b2b5d4a7eb47a8c206fe637f4d8227462f3b
Evgeny Cherkashin <eugeneai@irnok.net>
2022-06-26 12:32:35 +0800

    Now we account some hours

--- syllabus.lua
+++ syllabus.lua
@@ -126,6 +126,120 @@ function Topics:print()
    pt(self.labels, "LABELS:")
 end
 
+Works = {
+   works = {},
+   labels = {},
+   total = 0
+}
+
+Works.__index = Works
+setmetatable(Works, Topics)
+
+function Works:clear()
+   self.works={}
+   self.label={}
+   self.total=0
+   self.type = nil
+   self.name = nil
+   self.term = nil
+   self.comp = nil
+end
+
+
+function Works:getName(t)
+   if t=="lec" then
+      return "Лекция"
+   elseif t=="lab" then
+      return "Лабораторная работа"
+   elseif t=="per" then
+      return "Самостоятельная работа"
+   elseif t=="sem" then
+      return "Саминар"
+   elseif t=="pra" then
+      return "Практическая работа"
+   end
+end
+
+function Works:getRDFType(t)
+   pref = "wpdd:"
+   if t=="lec" then
+      return pref .. "Lection"
+   elseif t=="lab" then
+      return pref .. "LaboratoryWork"
+   elseif t=="per" or t=="ind" then
+      return pref .. "IndependentWork"
+   elseif t=="sem" then
+      return pref .. "Seminar"
+   elseif t=="pra" then
+      return pref .. "PracticalWork"
+   end
+end
+
+function Works:setType(t, name)
+   if t=='nil' then t=nil end
+   self.type = t
+   self:setName(name)
+end
+
+function Works:setName(name)
+   print(self)
+   if name then
+      self.name = name
+   else
+      self.name = self:getName(self.type)
+   end
+end
+
+function Works:sprintWorkName()
+   if self.name then
+      name = self.name
+   else
+      name = self:getName(self.type)  -- default if any
+   end
+   tex.sprint(string.format('\\def\\workname{%s}', name))
+end
+
+function Works:sprintRDFType()
+   tex.sprintRDFType("!" .. self:getRDFType(self.type))
+end
+
+function Works:addWork(w)
+   table.insert(self.works, w)
+   -- pt(w, "WORK")
+   self.total = self.total + w.hours
+   if w.label then
+      self.labels[w.label] = w
+   end
+   self.labels[w.index] = w
+end
+
+
+function Works:validation(total)
+   total = total or Topics.totalsControl[self.type]
+   -- pt(Topics.totalsControl, "TOP")
+   -- print(self.type)
+   if total == nil then
+      return error("Type is unknown and total is empty too")
+   end
+   if self.total ~= total then
+      tex.sprint(
+         string.format(
+            "\\paragraph{\\color{red} Количество часов не совпадает (%s) }", self.type))
+      d = self.total - total
+      if d>0 then
+         tex.sprint(
+            string.format(
+               "{\\bfseries \\color{red} суммарное количество (%u) БОЛЬШЕ целевого (%u) на %d}\\par",
+               self.total, total, d))
+      else
+         tex.sprint(
+            string.format(
+               "{\\bfseries \\color{red} суммарное количество (%u) МЕНЬШЕ целевого (%u) на %d}\\par",
+               self.total, total, -d))
+      end
+   end
+end
+
 Topic = {}
 
 function Topic:new(o)
@@ -146,7 +260,7 @@ function Topic:setTerm(term)
    self.year = (term+1) // 2
 end
 
-function Topic:hours(h)
+function Topic:setHours(h)
    update(self, h)
 end
 
@@ -167,8 +281,40 @@ function Topic:sprintTitle(emph)
    tex.sprint("\\par")
 end
 
+Work = {}
+
+function Work:new(o)
+   o = o or error("need a filled in object")
+   if o.kw then
+      update(o, o.kw)
+   end
+   o.kw = nil
+   self.__index = self
+   setmetatable(o, self)
+   setmetatable(self, Topic)
+   return o
+end
+
+
+
+function Work:setComp(comp)
+   if comp then
+      self.comp = comp
+   else
+      self.comp = Works.comp
+   end
+end
+
+function Work:setTopics(topics)
+   self.topics = topics
+end
+
+
+
 ALL.Topic = Topic
 ALL.Topics = Topics
+ALL.Works = Works
+ALL.Work = Work
 ALL.pt = pt
 
 return ALL
